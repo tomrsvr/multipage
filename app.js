a69e538861d8c59147585aadf116e94806efe6dc
tomrsvr <4099754+tomrsvr@users.noreply.github.com>
2023-01-17 10:35:54 +0900

    initial multipage version

---
+++ app.js
@@ -0,0 +1,114 @@
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.06em solid #646464}";
+    document.body.appendChild(css);
+};
+
+
+//Toggle theme — disabled while feature hidden
+// const themeBtn = document.querySelector('.theme-btn');
+// themeBtn.addEventListener('click',() =>{
+//     let element = document.body;
+//     element.classList.toggle('dark-mode');
+// })
+
+//Native animate on scroll (Fireship: https://www.youtube.com/watch?v=T33NN_pPeNI)
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        // if (entry.isIntersecting) {
+        //     entry.target.classList.add('show');
+        // } else {
+        //     entry.target.classList.remove('show');
+        // }
+        entry.target.classList.toggle('show', entry.isIntersecting); // Kudos @donniedamato YT
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+//Mobile menu
+
+let burger = document.getElementById('burger'),
+	 nav    = document.getElementById('main-nav');
+	//  slowmo = document.getElementById('slowmo');
+
+burger.addEventListener('click', function(e){
+	this.classList.toggle('is-open');
+	nav.classList.toggle('is-open');
+});
+
+// slowmo.addEventListener('click', function(e){
+// 	this.classList.toggle('is-slowmo');
+// });
+
+/* Onload demo - dirty timeout */
+let clickEvent = new Event('click');
+
+// window.addEventListener('load', function(e) {
+// 	// slowmo.dispatchEvent(clickEvent);
+// 	burger.dispatchEvent(clickEvent);
+	
+// 	setTimeout(function(){
+// 		burger.dispatchEvent(clickEvent);
+		
+// 		setTimeout(function(){
+// 			// slowmo.dispatchEvent(clickEvent);
+// 		}, 3500);
+// 	}, 5500);
+// });
+
+
